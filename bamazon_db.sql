70a5a3860016bb63e9be3933332b9da40d6f1243
JD <jdfili14@gmail.com>
2019-01-26 22:26:17 -0800

    db

--- bamazon_db.sql
+++ bamazon_db.sql
@@ -13,6 +13,6 @@ CREATE TABLE products (
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ('Cheese Wheel', 'Food', 10, 100), ('Healing Potion', 'Medicine', 50, 30),
-    ('Steel Sword', 'Weapons', 150, 5), ('Mithril Helm', 'Armor', 500, 2), ('Bronze Shield', 'Armor', 75, 10),
-    ('LongClaw (Specialized Sword)', 'Weapons', 1000, 1), ('Deer Meat', 'Food', 20, 75),('Mana Potion', 'Medicine', 45, 50),
+    ('Steel Sword', 'Weapons', 150, 5), ('Glass Helm', 'Armor', 500, 2), ('Bronze Shield', 'Armor', 75, 10),
+    ('Dope Sword (Specialized Sword)', 'Weapons', 1000, 1), ('Deer Meat', 'Food', 20, 75),('Mana Potion', 'Medicine', 45, 50),
     ('Bronze Axe', 'Weapons', 75, 20), ('Ale', 'Food', 15, 50);
