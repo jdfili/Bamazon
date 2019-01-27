4af703e461433494bec868963411dfe7f74d7b57
JD <jdfili14@gmail.com>
2019-01-26 22:32:58 -0800

    fixed db

--- bamazon_db.sql
+++ bamazon_db.sql
@@ -4,11 +4,11 @@ USE bamazon_db;
 
 CREATE TABLE products (
     item_id INT NOT NULL AUTO_INCREMENT,
-    product_name VARCHAR(20) NOT NULL,
+    product_name VARCHAR(100) NOT NULL,
     department_name VARCHAR(20) NOT NULL,
     price DECIMAL(10,2),
     stock_quantity INT(10),
-    PRIMARY KEY (id)
+    PRIMARY KEY (item_id)
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
